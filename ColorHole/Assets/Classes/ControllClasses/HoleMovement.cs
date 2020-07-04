cb2a60de92163c6e744964416a7420f73bb44cb7
Uğurcan Gökce <32913922+ugokce@users.noreply.github.com>
2020-07-04 17:12:46 +0300

    feat: implement event system and basic player controll class

---
+++ ColorHole/Assets/Classes/ControllClasses/HoleMovement.cs
@@ -0,0 +1,46 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class HoleMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody holeRigidbody;
+    private bool isMovingToAPoint = false;
+
+    void Start()
+    {
+        holeRigidbody = GetComponent<Rigidbody>();
+
+        EventManager.getInstance().playerEvents.onPlayerInput.AddListener(Move);
+        EventManager.getInstance().playerEvents.onDestinationTargetSelected.AddListener(MoveToPoint);
+    }
+
+    private void Move(Vector3 direction)
+    {
+        if(isMovingToAPoint)
+        {
+            return;
+        }
+
+        holeRigidbody.velocity = direction * moveSpeed;
+    }
+
+    private void MoveToPoint(Vector3 targetPoint)
+    {
+        transform.DOMove(targetPoint, 0.5f).SetEase(Ease.InOutQuad).OnStart(OnStartedMovingtoPoint).
+            OnComplete(OnCompletedMovingToPoint);
+    }
+
+    private void OnStartedMovingtoPoint()
+    {
+        isMovingToAPoint = true;
+    }
+
+    private void OnCompletedMovingToPoint()
+    {
+        isMovingToAPoint = false;
+    }
+}
---
+++ ColorHole/Assets/Classes/EventSystem/PlayerEvents.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventWithVector3 : UnityEvent<Vector3>
+{}
+
+public class EventWithFloat : UnityEvent<float>
+{}
+
+public class PlayerEvents
+{
+    public EventWithVector3 onPlayerInput;
+    public EventWithVector3 onDestinationTargetSelected;
+
+    public PlayerEvents()
+    {
+        onPlayerInput = new EventWithVector3();
+        onDestinationTargetSelected = new EventWithVector3();
+    }
+}
