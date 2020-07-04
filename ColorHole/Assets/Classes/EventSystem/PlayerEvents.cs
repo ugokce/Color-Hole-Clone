66ed4dacf67bddc0f15b2f2d59dc60de34c3c91e
Uğurcan Gökce <32913922+ugokce@users.noreply.github.com>
2020-07-04 17:35:18 +0300

    feat: implement gate class and create gate prefab

--- ColorHole/Assets/Classes/EventSystem/PlayerEvents.cs
+++ ColorHole/Assets/Classes/EventSystem/PlayerEvents.cs
@@ -9,14 +9,25 @@ public class EventWithVector3 : UnityEvent<Vector3>
 public class EventWithFloat : UnityEvent<float>
 {}
 
+public class EventWithInt : UnityEvent<int>
+{ }
+
 public class PlayerEvents
 {
     public EventWithVector3 onPlayerInput;
     public EventWithVector3 onDestinationTargetSelected;
+    public UnityEvent onGateOpened;
+    public UnityEvent onSubLevelCleared;
+
+    //completed level id
+    public EventWithInt onLevelCompleted;
 
     public PlayerEvents()
     {
         onPlayerInput = new EventWithVector3();
         onDestinationTargetSelected = new EventWithVector3();
+        onGateOpened = new UnityEvent();
+        onSubLevelCleared = new UnityEvent();
+        onLevelCompleted = new EventWithInt();
     }
 }
---
+++ ColorHole/Assets/Classes/LevelObjects/Gate.cs
@@ -0,0 +1,24 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    public GameObject gateObject;
+
+    void Start()
+    {
+        
+    }
+
+    private void OpenTheDoor()
+    {
+        gateObject.transform.DOMoveY(-1f, 0.3f).OnComplete(onDoorOpened);
+    }
+
+    private void onDoorOpened()
+    {
+        EventManager.getInstance().playerEvents.onGateOpened.Invoke();
+    }
+}
