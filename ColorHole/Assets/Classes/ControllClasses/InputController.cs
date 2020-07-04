aad62e9ecdf7b539b4373211950d2ceccb2c8c8b
Uğurcan Gökce <32913922+ugokce@users.noreply.github.com>
2020-07-04 18:07:48 +0300

    feat: implement mobile controller

---
+++ ColorHole/Assets/Classes/ControllClasses/InputController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    private Vector2 fingerMovementDir;
+
+    void Update()
+    {
+       if(Input.touchCount > 0)
+        {
+            if(Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                fingerMovementDir = Vector2.ClampMagnitude(Input.GetTouch(0).deltaPosition, 1);
+            }
+
+            if (Input.GetTouch(0).phase == TouchPhase.Stationary || Input.GetTouch(0).phase == TouchPhase.Stationary)
+            {
+                fingerMovementDir = Vector2.zero;
+            }
+
+            EventManager.getInstance().playerEvents.onPlayerInput.Invoke(new Vector3(fingerMovementDir.x, 0, fingerMovementDir.y));
+        }
+    }
+}
