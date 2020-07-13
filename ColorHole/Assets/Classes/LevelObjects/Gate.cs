9b91bae6b1c60ffc2199d433557c01d681e084ab
Uğurcan Gökce <32913922+ugokce@users.noreply.github.com>
2020-07-14 00:39:11 +0300

    feat: gate open and reset on level complete

--- ColorHole/Assets/Classes/LevelObjects/Gate.cs
+++ ColorHole/Assets/Classes/LevelObjects/Gate.cs
@@ -6,15 +6,23 @@ using UnityEngine;
 public class Gate : MonoBehaviour
 {
     public GameObject gateObject;
+    Vector3 initialPos;
 
     void Start()
     {
-        
+        initialPos = gateObject.transform.position;
+        EventManager.getInstance().playerEvents.onSubLevelCleared.AddListener(OpenTheDoor);
+        EventManager.getInstance().playerEvents.onLevelCompleted.AddListener(ResetDoor);
+    }
+
+    private void ResetDoor(int level)
+    {
+        gateObject.transform.position = initialPos;
     }
 
     private void OpenTheDoor()
     {
-        gateObject.transform.DOMoveY(-1f, 0.3f).OnComplete(onDoorOpened);
+        gateObject.transform.DOMoveY(-2.5f, 0.3f).OnComplete(onDoorOpened);
     }
 
     private void onDoorOpened()
